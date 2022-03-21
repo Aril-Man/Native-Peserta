3eec2b6197836d17b2ecc50fda0121ecbe78cdfc
Azriel Fauzi Hermansyah <azrielfhr23@gmail.com>
2022-03-21 12:05:11 +0700

    update

--- cetakpeserta.php
+++ cetakpeserta.php
@@ -1,58 +1,44 @@
-<?php 
+<?php
 include 'config/koneksi.php';
 include 'library/controller.php';
 
 
-    $go = new controller();
-    $table = "tbl_peserta";
-    @$field = array(
-        'nama_lengkap' => $_POST['nama_lengkap'],
-        'jk' => $_POST['jk'],
-        'alamat' => $_POST['alamat'],
-        'agama' => $_POST['agama'],
-        'asal_smp' => $_POST['asal_smp'],
-        'jurusan' => $_POST['jurusan']
-    );
+$go = new controller();
+$table = "tbl_peserta";
+@$field = array(
+    'nama_lengkap' => $_POST['nama_lengkap'],
+    'jk' => $_POST['jk'],
+    'alamat' => $_POST['alamat'],
+    'agama' => $_POST['agama'],
+    'asal_smp' => $_POST['asal_smp'],
+    'jurusan' => $_POST['jurusan']
+);
 
-    @$where = "no_daftar= $_GET[no_daftar]";
+@$where = "no_daftar= $_GET[no_daftar]";
 
-    if (isset($_GET['edit'])) {
-        $edit = $go->edit($con, $table, $where);
-    }
+if (isset($_GET['edit'])) {
+    $edit = $go->edit($con, $table, $where);
+}
 
 ?>
+<?php
+$sql = "SELECT * FROM tbl_peserta ";
+$go = mysqli_query($con, $sql);
+while ($r = mysqli_fetch_array($go)) {
+?>
 
-<div style="padding:10px;">
-    <div class="table-responsive">
-        <table align="center" border="1" class="mt-4 table table-stripped table-hover bg-white" id="data">
-            <tr>
-                <th>No Dafrar</th>
-                <th>Nama Lengkap</th>
-                <th>Jenis Kelamin</th>
-                <th>Alamat</th>
-                <th>Agama</th>
-                <th>Asal SMP</th>
-                <th>Jurusan</th>
-            </tr>
-            <?php
-                $sql = "SELECT * FROM tbl_peserta ";
-                $go = mysqli_query($con, $sql);
-                while($r = mysqli_fetch_array($go)){
-            ?>
-            <tr>
-                <td><?php echo $r['no_daftar']?></td>
-                <td><?php echo $r['nama_lengkap']?></td>
-                <td><?php echo $r['jk']?></td>
-                <td><?php echo $r['alamat']?></td>
-                <td><?php echo $r['agama']?></td>
-                <td><?php echo $r['asal_smp'] ?></td>
-                <td><?php echo $r['jurusan']?></td>
-            </tr>
-            <?php } ?>
-        </table>
-    </div>
-</div>
+<p> <?php echo $r['no_daftar'] ?></p>
+<p><?php echo $r['nama_lengkap'] ?></p>
+<p><?php echo $r['jk'] ?></p>
+<p> <?php echo $r['alamat'] ?></p>
+<p><?php echo $r['agama'] ?></p>
+<p><?php echo $r['asal_smp'] ?></p>
+<p><?php echo $r['jurusan'] ?></p>
+
+<?php } ?>
+
+
 
 <script type="text/javascript">
-        window.print();
+window.print();
 </script>
